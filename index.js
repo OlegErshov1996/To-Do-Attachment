a90d115de3518d5878a8bdf1e00ae6ee247dd495
OlegErshov1996 <oleg_ershov1996@mail.ru>
2020-03-19 23:54:02 +0300

    Разработка To Do, Подключение MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+const handlebars = expressHandlebars.create({
+    defaultLayout: 'main',
+    extname: 'handlebars'
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('views', 'views');
 
 async function start() {
     try {
-        await mongoose.connect('', {
+        await mongoose.connect('mongodb+srv://Oleg:971971555@cluster0-zo8tx.mongodb.net/todo', {
             useNewUrlParser: true,
             useFindAndModify: false
-        })
+        });
         app.listen(PORT, () => {
             console.log('Сервер запущен');
         })
